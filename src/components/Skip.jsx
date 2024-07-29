aa2e99eaf8cb98aa41e7f3d72386690566ee9f79
wooseoyo <0420hjk@gmail.com>
2024-07-29 12:11:26 +0900

    react_sample
    react_sample

---
+++ src/components/Skip.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Skip = () => {
+    return (
+        <div id='skip'>
+            <a href="#header">헤더영역바로가기</a>
+            <a href="#intro">인트로영역바로가기</a>
+            <a href="#skill">스킬영역바로가기</a>
+            <a href="#site">사이트영역바로가기</a>
+            <a href="#port">포트폴리오영역바로가기</a>
+            <a href="#contact">연락처영역바로가기</a>
+            <a href="#푸터">푸터영역바로가기</a>
+        </div>
+    )
+}
+
+export default Skip
